30803a613eedfad1a229c40ece32b51591001581
asaduzzaman shahin <ashahin1995@gmail.com>
2024-01-15 23:54:52 +0600

    Initial commit

---
+++ lib/DataSources.dart
@@ -0,0 +1,19 @@
+class DataSources {
+  static List myNews = [
+    {
+      "headline": "Scientists Make Strides in Clean Energy",
+      "image": "https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2023/10/breaking-latest-1698021693.jpg",
+      "content": "In a groundbreaking development, scientists have made significant strides in clean energy research, paving the way for a sustainable future."
+    },
+    {
+      "headline": "Global Space Mission Discovers New Exoplanet",
+      "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Fox_News_Channel_logo.svg/1200px-Fox_News_Channel_logo.svg.png",
+      "content": "A global space mission has identified a new exoplanet in a distant galaxy, offering valuable insights into the mysteries of the universe."
+    },
+    {
+      "headline": "Tech Company Launches Next-Gen Smartphone",
+      "image": "https://t4.ftcdn.net/jpg/03/37/92/49/360_F_337924964_fF6I0iZkv9lvoIEn71f0OsgawArIPPDu.jpg",
+      "content": "Tech enthusiasts rejoice as a leading company launches a cutting-edge smartphone, boasting revolutionary features and enhanced performance."
+    }
+  ];
+}
